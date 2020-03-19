1b0c902a3e63f6e42b0ba7dded0fd7f74d41876f
panagiwthskan <panagiwthskan@gmail.com>
2020-03-19 13:51:45 +0200

    added TimelineItem component

---
+++ src/lib/TimelineItem/TimelineItem.js
@@ -0,0 +1,266 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Card, Grid, CardMedia, CardContent, Box, Paper, makeStyles, Divider, Collapse, CardActions, IconButton } from '@material-ui/core';
+import {directions} from '../../enums/enums'
+
+export const useStyles = makeStyles(theme => {
+	return {
+		TimelineItem: {
+			width: props => props.timelineItemWidth
+				? props.timelineItemWidth : '50%',
+			alignContent: 'center',
+			display: 'flex'
+		},
+		TimelineItemFull: {
+			width: props => props.timelineItemWidth
+				? props.timelineItemWidth : '100%',
+			alignSelf: 'flex-start',
+			flexDirection: 'row',
+			display: 'flex',
+		},
+		TimelineCard: {
+			marginLeft: '-1em',
+		},
+		TimelineCardContent: {
+			paddingTop: props => props.padTop ? '8px' : null
+		},
+		ArrowLeft: {
+			display: 'flex',
+			flexDirection: 'row',
+			marginTop: theme.spacing(2),
+			direction: 'ltr',
+			marginLeft: (props) => !props.isOneWay ? '64px' : '0',
+			'&::before': {
+				content: '" "',
+				width: 0,
+				height: 0,
+				borderTop: '0.5em solid white',
+				borderBottom: '0.5em solid white',
+				borderLeft: '0.5em solid white',
+				borderRight: '0.5em solid white',
+				boxShadow: '-1px 2px 1px -1px rgba(0,0,0,0.4)',
+				transformOrigin: '0 0',
+				transform: 'rotate(45deg)',
+				marginLeft: '1em'
+			}
+		},
+		ArrowRight: {
+			display: 'flex',
+			flexDirection: 'row-reverse',
+			marginTop: theme.spacing(2),
+			direction: 'ltr',
+			marginRight: (props) => !props.isOneWay ? '64px' : '0',
+			'&::before': {
+				content: '" "',
+				width: 0,
+				height: 0,
+				borderTop: '0.5em solid white',
+				borderBottom: '0.5em solid white',
+				borderLeft: '0.5em solid white',
+				borderRight: '0.5em solid white',
+				boxShadow: '2px -1px 1px -1px rgba(0,0,0,0.4)',
+				transformOrigin: '0 0',
+				transform: 'rotate(45deg)',
+			},
+
+		},
+		timelineYearWrapper: {
+			content: '" "',
+			background: (props) => 'linear-gradient(' + props.yearBackgroundColor + ',' + props.yearBackgroundColor + ' ) no-repeat center/4px 100%',
+			height: '100%',
+			display: 'block',
+		},
+		timelineYear: {
+			alignSelf: 'flex-start',
+			padding: 16,
+			backgroundColor: props => props.yearBackgroundColor,
+			color: props => props.yearColor,
+		},
+		timelineItemHeader: {
+			padding: theme.spacing(1),
+			display: 'flex',
+			flexDirection: 'row',
+			justifyContent: 'center'
+		},
+		cardMedia: {
+			height: props => props.cardMediaProps.height,
+			objectFit: "contain"
+		},
+		expand: {
+			transform: 'rotate(0deg)',
+			marginLeft: 'auto',
+			transition: theme.transitions.create('transform', {
+				duration: theme.transitions.duration.shortest,
+			}),
+		},
+		expandOpen: {
+			transform: 'rotate(180deg)',
+		},
+		listDotLeft: {
+			position: 'absolute',
+			left: '50%',
+			transform: 'translate(-50%, 0)',
+			height: '100%'
+		},
+		listDotRight: {
+			position: 'absolute',
+			right: '50%',
+			transform: 'translate(50%, 0)',
+			height: '100%'
+		}
+	}
+})
+
+const TimelineItem = props => {
+
+	const [expanded, setExpanded] = useState(false)
+
+	const handleExpandClick = () => {
+		setExpanded(!expanded);
+	}
+
+	const classes = useStyles(props);
+
+	const cardMedia = props.cardMediaProps ? (
+		<CardMedia className={classes.cardMedia} component='img' src={props.cardMediaProps.imgUrl} />
+	) : null;
+
+	const cardHeader = props.titleChildren ||
+		props.cardHeaderChildren ? (
+			<Grid container direction='column'>
+				{props.titleChildren ? (
+					<Grid item xs={11} md={11}>
+						{props.titleChildren}
+					</Grid>
+				) : null}
+
+				{props.cardHeaderChildren ? (
+					<Grid item xs={11} md={11}>
+						<Grid container direction='row'>
+							{props.cardHeaderChildren}
+						</Grid>
+					</Grid>) : null}
+			</Grid>
+		) : null;
+
+	let cardContent = props.cardContentChildren ? (
+		<CardContent>
+			{props.cardContentChildren}
+		</CardContent>
+	) : null;
+
+	if (props.expandableCardContent && props.cardContentChildren) {
+		cardContent = (
+			<React.Fragment>
+				<CardActions>
+					<IconButton
+						className={expanded ?
+							[classes.expand, classes.expandOpen].join(' ') :
+							[classes.expand].join(' ')}
+						onClick={handleExpandClick}
+						aria-expanded={expanded}
+						aria-label="show more">
+						<ExpandMoreIcon />
+					</IconButton>
+				</CardActions>
+				<Collapse in={expanded} timeout="auto">
+					{cardContent}
+				</Collapse>
+			</React.Fragment>
+		)
+	}
+
+	let finalItem;
+	if (!props.isOneWay) {
+		finalItem = (
+			<div className={classes.TimelineItem}>
+				<div className={props.direction === directions.LEFT ? classes.listDotLeft : classes.listDotRight}>
+					<Paper className={classes.timelineYear}>
+						{props.iconContent}
+					</Paper>
+					<div className={classes.timelineYearWrapper}>
+
+					</div>
+				</div>
+				<Box className={props.direction === directions.LEFT ? classes.ArrowLeft : classes.ArrowRight}>
+					<Card component='div' className={classes.TimelineCard}>
+						<Grid container justify='space-evenly'>
+							<Grid item xs={3} md={3}>
+								{cardMedia}
+							</Grid>
+							<Grid className={classes.TimelineCardContent} item xs={8} md={8}>
+								{cardHeader}
+							</Grid>
+							{props.hasDivider ? (
+								<Grid item xs={12} md={12}>
+									<br />
+									<Divider />
+								</Grid>
+							) : null}
+							<Grid item xs={12} md={12}>
+								{cardContent}
+							</Grid>
+						</Grid>
+					</Card>
+				</Box>
+			</div>
+		)
+	}
+	else {
+
+		finalItem = (
+			<div className={classes.TimelineItemFull}>
+				<div>
+					<Paper className={classes.timelineYear}>
+						{props.iconContent}
+					</Paper>
+					<div className={classes.timelineYearWrapper}>
+
+					</div>
+				</div>
+				<Box className={props.direction === directions.LEFT ? classes.ArrowLeft : classes.ArrowRight}>
+					<Card component='div' className={classes.TimelineCard}>
+						<Grid container justify='space-evenly'>
+							<Grid item xs={3} md={3}>
+								{cardMedia}
+							</Grid>
+							<Grid className={classes.TimelineCardContent} item xs={8} md={8}>
+								{cardHeader}
+							</Grid>
+							{props.hasDivider ? (
+								<Grid item xs={12} md={12}>
+									<br />
+									<Divider />
+								</Grid>
+							) : null}
+							<Grid item xs={12} md={12}>
+								{cardContent}
+							</Grid>
+						</Grid>
+					</Card>
+				</Box>
+			</div>
+		)
+	}
+	return finalItem;
+};
+
+
+const TimelineItemPropTypes = {
+	yearBackgroundColor: PropTypes.string,
+	yearColor: PropTypes.string,
+	iconContent: PropTypes.element,
+	cardHeaderChildren: PropTypes.element,
+	cardContentChildren: PropTypes.element,
+	cardMediaProps: PropTypes.object.isRequired,
+	expandableCardContent: PropTypes.bool,
+	title: PropTypes.string,
+	padTop: PropTypes.bool,
+	timelineItemWidth: PropTypes.string,
+	hasDivider: PropTypes.bool,
+};
+
+TimelineItem.propTypes = TimelineItemPropTypes;
+
+export default TimelineItem;
---
+++ src/lib/TimelineItem/TimelineItem.test.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import TimelineItem from './TimelineItem';
+import { renderIntoDocument, scryRenderedDOMComponentsWithClass } from 'react-dom/test-utils';
+import { directions } from '../../enums/enums';
+import { render } from '@testing-library/react';
+
+
+describe('TimelineItem', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      titleChildren:(<div></div>),
+      yearBackgroundColor:'#fafafa',
+      yearColor: 'white',
+      cardContentChildren: (<div></div>),
+      cardHeaderChildren: (<div></div>),
+      iconContent: (<div></div>),
+      cardMediaProps: {src: ''},
+      expandableCardContent: true,
+      padTo: true,
+      hasDivider: true,
+    }
+  });
+
+  // Shallow / unit tests begin here
+  it('Should render component with Full Element', () => {
+    props = {
+      ...props,
+      direction: directions.LEFT,
+      isOneWay: true,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)
+    expect(container.firstChild.classList.toString().includes('TimelineItemFull')).toBe(true)
+
+  })
+  
+  it('Should render component with Simple Element', () => {
+    props = {
+      ...props,
+      direction: directions.LEFT,
+      isOneWay: false,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)
+    expect(container.firstChild.classList.toString().includes('TimelineItemFull')).toBe(false)
+
+  })
+
+  it('Should render component with Simple Element (default value)', () => {
+    props = {
+      ...props,
+      direction: directions.LEFT,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)
+    expect(container.firstChild.classList.toString().includes('TimelineItemFull')).toBe(false)
+
+  })
+
+  it('Should render component with Full Element with Left Arrow', () => {
+    props = {
+      ...props,
+      direction: directions.LEFT,
+      isOneWay: true,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)
+    expect(container.firstChild.children[1].classList.toString().includes('ArrowLeft')).toBe(true)
+  })
+  
+  it('Should render component with Full Element with Right Arrow', () => {
+    props = {
+      ...props,
+      direction: directions.RIGHT,
+      isOneWay: true,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)
+    expect(container.firstChild.children[1].classList.toString().includes('ArrowRight')).toBe(true)
+
+  })
+
+  it('Should render component with Full Element with Right Arrow (default value)', () => {
+    props = {
+      ...props,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)
+    expect(container.firstChild.children[1].classList.toString().includes('ArrowRight')).toBe(true)
+  })
+  
+  it('Should render component with Element with List Dot Left', () => {
+    props = {
+      ...props,
+      direction: directions.LEFT,
+      isOneWay: false,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)
+    expect(container.firstChild.children[0].classList.toString().includes('listDotLeft')).toBe(true)
+    
+  })
+  
+  it('Should render component with Element with List Dot Right', () => {
+    props = {
+      ...props,
+      direction: directions.RIGHT,
+      isOneWay: false,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)    
+    expect(container.firstChild.children[0].classList.toString().includes('listDotRight')).toBe(true)
+
+
+  })
+  
+  it('Should render component with Element with List Dot Right (default value)', () => {
+    props = {
+      ...props,
+    }
+
+    const {container} = render(<TimelineItem {...props} />)    
+    expect(container.firstChild.children[0].classList.toString().includes('listDotRight')).toBe(true)
+
+
+  })
+
+  //TODO: check if the tests above work with no input (default values)
+  // Render / mount / integration tests begin here
+
+});
