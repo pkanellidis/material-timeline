ffe8d0babf26e475260ee423216a7c58a0f5a19b
pk12 <panagiwthskan@gmail.com>
2020-04-08 20:29:25 +0300

    removed exports added directions export

--- src/lib/TimelineItem/TimelineItem.d.ts
+++ src/lib/TimelineItem/TimelineItem.d.ts
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 
-export interface TimelineItemProps {
+interface TimelineItemProps {
     yearBackgroundColor: string,
     yearColor: string,
     iconContent: PropTypes.ReactNodeLike,
